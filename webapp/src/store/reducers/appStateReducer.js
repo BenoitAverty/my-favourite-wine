944809ed6605a975cf1b6324b2c8d72adc5224de
Benoit AVERTY <benoit.averty@gmail.com>
2016-05-22 21:26:36 +0200

    Implemented reducer for the 'Change search String' action

--- webapp/src/store/reducers/appStateReducer.js
+++ webapp/src/store/reducers/appStateReducer.js
@@ -1,4 +1,4 @@
-import { SEARCH_WINE } from '../../actions';
+import { SEARCH_WINE, CHANGE_SEARCH_STRING } from '../../actions';
 
 const initialState = {
   label: 'MAIN_PAGE',
@@ -8,6 +8,8 @@ const appStateReducer = (appState = initialState, action) => {
   switch (action.type) {
     case SEARCH_WINE:
       return { label: 'SEARCHING_WINE' };
+    case CHANGE_SEARCH_STRING:
+      return { label: appState.label, searchString: action.newSearchString };
     default:
       return appState;
   }
--- webapp/test/app-tests/reducers/AppStateReducer.spec.js
+++ webapp/test/app-tests/reducers/AppStateReducer.spec.js
@@ -3,7 +3,7 @@
 import { expect } from 'chai';
 
 import appStateReducer from '../../../src/store/reducers/appStateReducer';
-import { searchWine } from '../../../src/actions';
+import { searchWine, changeSearchString } from '../../../src/actions';
 
 describe('stateReducer', () => {
   describe('When receiving a searchWine action', () => {
@@ -13,6 +13,24 @@ describe('stateReducer', () => {
       const expected = { label: 'SEARCHING_WINE' };
 
       expect(actual).to.deep.equal(expected);
+    });
+  });
+
+  describe('When receiving a changeSearchString action', () => {
+    it('Should return a new state with the searchString changed to the value of the aciton', () => {
+      const action = changeSearchString('new value');
+      const { searchString: actual } = appStateReducer({ searchString: 'a search string' }, action);
+      const expected = 'new value';
+
+      expect(actual).to.equal(expected);
+    });
+
+    it('Should return a state with the same appState label', () => {
+      const action = changeSearchString('new value');
+      const { label } = appStateReducer({ label: 'A LABEL', searchString: 'searchString' }, action);
+      const expected = 'A LABEL';
+
+      expect(label).to.equal(expected);
     });
   });
 
